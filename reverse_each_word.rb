f72fcf2d96b351305a6ece41ed2e345e9837b9a7
Brandon Yoon <byoon5397@gmail.com>
2020-03-28 21:28:07 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,6 @@ def reverse_each_word(sent)
   words = sent.split(" ")
   new_sent = []
   words.each do { |word| new_sent << word.reverse }
-  new_sent.join(" ")
-  new_sent
+  reversed = new_sent.join(" ")
+  reversed
 end
